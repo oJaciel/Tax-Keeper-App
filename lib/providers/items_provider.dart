5c6d80a748b6bd923fe68578c5a16460edaacbe5
oJaciel <jacielhbarichellO@gmail.com>
2025-03-11 21:28:16 -0300

    Formulário + Método POST Completos
    Adicionados os campos faltantes no formulário, bem como no método de adicionar produto do Provider.

--- lib/providers/items_provider.dart
+++ lib/providers/items_provider.dart
@@ -22,6 +22,16 @@ class ItemsProvider with ChangeNotifier {
       name: data['name'] as String,
       ncm: data['ncm'] as String,
       imageUrl: data['imageUrl'] as String,
+      codigoBarras: data['codigoBarras'] as String,
+      cest: data['cest'] as String,
+      cstIcms: data['cstIcms'] as String,
+      aliquotaIcms: data['aliquotaIcms'] as double,
+      cstIpi: data['cstIpi'] as String,
+      aliquotaIpi: data['aliquotaIpi'] as double,
+      cstPis: data['cstPis'] as String,
+      aliquotaPis: data['aliquotaPis'] as double,
+      cstCofins: data['cstCofins'] as String,
+      aliquotaCofins: data['aliquotaCofins'] as double,
     );
 
     return addItem(item);
@@ -34,13 +44,38 @@ class ItemsProvider with ChangeNotifier {
         "name": item.name,
         "ncm": item.ncm,
         "imageUrl": item.imageUrl,
+        "codigoBarras": item.codigoBarras,
+        "cest": item.cest,
+        "cstIcms": item.cstIcms,
+        "aliquotaIcms": item.aliquotaIcms,
+        "cstIpi": item.cstIpi,
+        "aliquotaIpi": item.aliquotaIpi,
+        "cstPis": item.cstPis,
+        "aliquotaPis": item.aliquotaPis,
+        "cstCofins": item.cstCofins,
+        "aliquotaCofins": item.aliquotaCofins,
       }),
     );
 
     final id = jsonDecode(response.body)['name'];
 
     _items.add(
-      Item(id: id, name: item.name, ncm: item.ncm, imageUrl: item.imageUrl),
+      Item(
+        id: id,
+        name: item.name,
+        ncm: item.ncm,
+        imageUrl: item.imageUrl,
+        codigoBarras: item.codigoBarras,
+        cest: item.cest,
+        cstIcms: item.cstIcms,
+        aliquotaIcms: item.aliquotaIcms,
+        cstIpi: item.cstIpi,
+        aliquotaIpi: item.aliquotaIpi,
+        cstPis: item.cstPis,
+        aliquotaPis: item.aliquotaPis,
+        cstCofins: item.cstCofins,
+        aliquotaCofins: item.aliquotaCofins,
+      ),
     );
 
     notifyListeners();
--- lib/screens/item_form_screen.dart
+++ lib/screens/item_form_screen.dart
@@ -14,35 +14,35 @@ class _ItemFormScreenState extends State<ItemFormScreen> {
   final _formData = Map<String, Object>();
 
   Future<void> _submitForm() async {
-  if (!_formKey.currentState!.validate()) {
-    return;
-  }
-  _formKey.currentState!.save(); // Aqui garantimos que _formData está preenchido
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    _formKey.currentState!.save();
 
-  try {
-    await Provider.of<ItemsProvider>(
-      context,
-      listen: false,
-    ).saveItem(_formData);
-  } catch (error) {
-    await showDialog<void>(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: Text('Erro!'),
-        content: Text(error.toString()),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: Text('Ok'),
-          ),
-        ],
-      ),
-    );
-  } finally {
-    Navigator.of(context).pop();
+    try {
+      await Provider.of<ItemsProvider>(
+        context,
+        listen: false,
+      ).saveItem(_formData);
+    } catch (error) {
+      await showDialog<void>(
+        context: context,
+        builder:
+            (ctx) => AlertDialog(
+              title: Text('Erro!'),
+              content: Text(error.toString()),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Ok'),
+                ),
+              ],
+            ),
+      );
+    } finally {
+      Navigator.of(context).pop();
+    }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -57,25 +57,163 @@ class _ItemFormScreenState extends State<ItemFormScreen> {
           key: _formKey,
           child: ListView(
             children: [
-              //Campo de nome
+              // Campo de Nome
               TextFormField(
                 decoration: InputDecoration(labelText: 'Nome'),
                 textInputAction: TextInputAction.next,
-                onSaved: (name) => _formData['name'] = name ?? '',
+                onSaved: (name) => _formData['name'] = name ?? 'N/A',
               ),
-              //Campo de NCM
+
+              // Campo de NCM
               TextFormField(
                 decoration: InputDecoration(labelText: 'NCM'),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.numberWithOptions(),
-                onSaved: (ncm) => _formData['ncm'] = ncm ?? '',
+                onSaved: (ncm) => _formData['ncm'] = ncm ?? 'N/A',
               ),
-              //Campo de URL da imagem
+
+              // Campo de Código de Barras
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Código de Barras'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                onSaved:
+                    (codigoBarras) =>
+                        _formData['codigoBarras'] = codigoBarras ?? 'N/A',
+              ),
+
+              // Campo de CEST
+              TextFormField(
+                decoration: InputDecoration(labelText: 'CEST'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                onSaved: (cest) => _formData['cest'] = cest ?? 'N/A',
+              ),
+
+              // CST ICMS e Alíquota ICMS lado a lado
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'CST ICMS'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.number,
+                      onSaved:
+                          (cstIcms) => _formData['cstIcms'] = cstIcms ?? 'N/A',
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Alíquota ICMS'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      onSaved:
+                          (aliquotaIcms) =>
+                              _formData['aliquotaIcms'] =
+                                  double.tryParse(aliquotaIcms ?? '') ?? 0.0,
+                    ),
+                  ),
+                ],
+              ),
+
+              // CST IPI e Alíquota IPI lado a lado
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'CST IPI'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.number,
+                      onSaved:
+                          (cstIpi) => _formData['cstIpi'] = cstIpi ?? 'N/A',
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Alíquota IPI'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      onSaved:
+                          (aliquotaIpi) =>
+                              _formData['aliquotaIpi'] =
+                                  double.tryParse(aliquotaIpi ?? '') ?? 0.0,
+                    ),
+                  ),
+                ],
+              ),
+
+              // CST PIS e Alíquota PIS lado a lado
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'CST PIS'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.number,
+                      onSaved:
+                          (cstPis) => _formData['cstPis'] = cstPis ?? 'N/A',
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Alíquota PIS'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      onSaved:
+                          (aliquotaPis) =>
+                              _formData['aliquotaPis'] =
+                                  double.tryParse(aliquotaPis ?? '') ?? 0.0,
+                    ),
+                  ),
+                ],
+              ),
+
+              // CST COFINS e Alíquota COFINS lado a lado
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'CST COFINS'),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.number,
+                      onSaved:
+                          (cstCofins) =>
+                              _formData['cstCofins'] = cstCofins ?? 'N/A',
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Alíquota COFINS'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      onSaved:
+                          (aliquotaCofins) =>
+                              _formData['aliquotaCofins'] =
+                                  double.tryParse(aliquotaCofins ?? '') ?? 0.0,
+                    ),
+                  ),
+                ],
+              ),
+
+              // Campo de URL da imagem
               TextFormField(
                 decoration: InputDecoration(labelText: 'URL da Imagem'),
                 textInputAction: TextInputAction.done,
                 keyboardType: TextInputType.url,
-                onSaved: (imageUrl) => _formData['imageUrl'] = imageUrl ?? '',
+                onSaved:
+                    (imageUrl) => _formData['imageUrl'] = imageUrl ?? 'N/A',
               ),
             ],
           ),
