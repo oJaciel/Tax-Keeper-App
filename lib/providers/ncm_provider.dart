7eb3062344b132f2772ef78630504f75ca0180a5
oJaciel <jacielhbarichellO@gmail.com>
2025-04-18 21:41:09 -0300

    Método Carregar NCM
    Criado método que carrega apenas os NCM dos itens cadastrados.
    
    Método implementado na tela de NCM parcialmente.
    
    Descontinuado o método que retornava todos os NCM existentes.

--- lib/providers/ncm_provider.dart
+++ lib/providers/ncm_provider.dart
@@ -1,30 +1,66 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:tax_keeper/models/item.dart';
 import 'package:tax_keeper/utils/constants.dart';
 
 class NcmProvider with ChangeNotifier {
-  List<Map<String, String>> _ncm_list = [];
+  List<Map<String, dynamic>> _ncm_list = [];
 
-  List<Map<String, String>> get ncm_list {
+  List<Map<String, dynamic>> get ncm_list {
     return [..._ncm_list];
   }
 
-  Future<void> loadNcm() async {
+
+
+  Future<void> loadItemsNcm(List<Item> items) async {
     _ncm_list.clear();
 
-    final response = await http.get(Uri.parse(Constants.NCM_API_BASE_URL));
+    for (var item in items) {
+      final itemNcm = item.ncm.replaceAll('.', '');
 
-    if (response.body == 'null' || response.body.isEmpty) {
-      return;
-    }
+      if (itemNcm.isEmpty) {
+        continue; //Se NCM for vazio, ignora o item
+      }
 
-    final List<dynamic> data = jsonDecode(response.body);
+      final response = await http.get(
+        Uri.parse('${Constants.NCM_API_BASE_URL}/$itemNcm'),
+      );
 
-    for (var item in data) {
-      _ncm_list.add({'codigo': item['codigo'], 'descricao': item['descricao']});
+      if (response.body == 'null' || response.body.isEmpty) {
+        continue; // Se a resposta for vazia ou nula, ignora o item
+      }
+
+      final Map<String, dynamic> ncmData = jsonDecode(response.body);
+
+      // Verificar se 'codigo' ou 'descricao' são null, caso sejam, atribuir valores padrão
+      _ncm_list.add({
+        //Adicionando na lista o item passado, o código NCM e a descrição do NCM
+        'item': item,
+        'codigo':
+            ncmData['codigo'] ??
+            'Código não disponível', 
+        'descricao':
+            ncmData['descricao'] ??
+            'Descrição não disponível', 
+      });
     }
 
     notifyListeners();
   }
 }
+
+
+
+  // Future<void> loadAllNcm() async {
+  //   _ncm_list.clear();
+  //   final response = await http.get(Uri.parse(Constants.NCM_API_BASE_URL));
+  //   if (response.body == 'null' || response.body.isEmpty) {
+  //     return;
+  //   }
+  //   final List<dynamic> data = jsonDecode(response.body);
+  //   for (var item in data) {
+  //     _ncm_list.add({'codigo': item['codigo'], 'descricao': item['descricao']});
+  //   }
+  //   notifyListeners();
+  // }
--- lib/screens/ncm_screen.dart
+++ lib/screens/ncm_screen.dart
@@ -1,44 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tax_keeper/components/app_drawer.dart';
+import 'package:tax_keeper/models/item.dart';
+import 'package:tax_keeper/providers/items_provider.dart';
 import 'package:tax_keeper/providers/ncm_provider.dart';
 
-class NcmScreen extends StatelessWidget {
+class NcmScreen extends StatefulWidget {
   const NcmScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final ncmProvider = Provider.of<NcmProvider>(context, listen: false);
+  State<NcmScreen> createState() => _NcmScreenState();
+}
 
+class _NcmScreenState extends State<NcmScreen> {
+  bool _isInit = true;
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      final items = Provider.of<ItemsProvider>(context, listen: false).items;
+
+      Future.microtask(() {
+        Provider.of<NcmProvider>(context, listen: false).loadItemsNcm(items);
+      });
+
+      _isInit = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<NcmProvider>(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Consulta de NCM')),
       drawer: AppDrawer(),
-      body: FutureBuilder(
-        future: ncmProvider.loadNcm(),
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          return Consumer<NcmProvider>(
-            builder: (ctx, provider, _) {
-              final list = provider.ncm_list;
-
-              if (list.isEmpty) {
-                return const Center(child: Text('Nenhum NCM encontrado.'));
-              }
-
-              return ListView.builder(
-                itemCount: list.length,
-                itemBuilder:
-                    (ctx, i) => ListTile(
-                      title: Text(list[i]['codigo'] ?? ''),
-                      subtitle: Text(list[i]['descricao'] ?? ''),
-                    ),
-              );
-            },
-          );
-        },
+      body: Column(
+        children: [
+          provider.ncm_list.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : Expanded(
+                child: ListView.builder(
+                  itemCount: provider.ncm_list.length,
+                  itemBuilder: (context, index) {
+                    final Item item = provider.ncm_list[index]['item'] ?? '';
+                    return ListTile(
+                      
+                      title: Text(item.name),
+                      subtitle: Text('${provider.ncm_list[index]['codigo'] ?? ''} - ${provider.ncm_list[index]['descricao'] ?? ''}'),
+                    );
+                  },
+                ),
+              ),
+        ],
       ),
     );
   }
